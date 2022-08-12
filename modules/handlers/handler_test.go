72062f95ebf31a8d31716a23a6346773458c7499
chaewonkong <chaewonkong@nexon.co.kr>
2022-08-12 13:54:16 +0900

    test: add panic case for wrong id provided

--- modules/handlers/handler_test.go
+++ modules/handlers/handler_test.go
@@ -3,6 +3,7 @@ package handlers_test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strconv"
 	"testing"
 	mocks "todo-go/mocks/repositories"
 	"todo-go/modules/domains"
@@ -10,6 +11,7 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func fixtures() (h *handlers.Handler, m *mocks.Repository) {
@@ -20,7 +22,8 @@ func fixtures() (h *handlers.Handler, m *mocks.Repository) {
 }
 
 func TestFindOne(t *testing.T) {
-	responseJSON := `{"id":0,"title":"title1","checked":false,"created":"0001-01-01T00:00:00Z","updated":"0001-01-01T00:00:00Z"}`
+	responseJSON := `{"id":1,"title":"title1","checked":false,"created":"0001-01-01T00:00:00Z","updated":"0001-01-01T00:00:00Z"}`
+	id := 1
 
 	h, m := fixtures()
 	e := echo.New()
@@ -30,12 +33,27 @@ func TestFindOne(t *testing.T) {
 
 	c := e.NewContext(req, rec)
 	c.SetParamNames("id")
-	c.SetParamValues("1")
+	c.SetParamValues(strconv.Itoa(id))
 
-	item := domains.Item{Title: "title1"}
-	m.On("FindOne", uint64(1)).Return(item, nil)
+	item := domains.Item{Title: "title1", ID: uint64(1)}
+
+	idMatcher := func(id uint64) bool {
+		return id == item.ID
+	}
+
+	matchedBy := mock.MatchedBy(idMatcher)
+	m.On("FindOne", matchedBy).Return(item, nil)
+
+	// Correct id provided
 	if assert.NoError(t, h.FindOne(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
 		assert.JSONEq(t, responseJSON, rec.Body.String())
 	}
+
+	// Incorrect id provided
+	assert.Panics(t, func() {
+
+		c.SetParamValues("2")
+		assert.Error(t, h.FindOne(c))
+	})
 }
